8c77919ac4d1b5c9f6cfae5c899d14a1d79d1e8a
Ivan lopez <42913512+Ivandariotv@users.noreply.github.com>
2024-01-24 22:14:18 -0500

    feat: banner principal en proceso

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,4 +1,29 @@
 <script setup lang="ts">
 </script>
 
-<template></template>
+<template>
+  <div class="h-[75%] bg-contain bg-no-repeat bg-right mx-[15%] mt-[3%] grid content-between py-12"
+    style="background-image: url('../src/assets/images/profile.png');">
+    <div>
+      <label
+        class="mb-4 inline-flex items-center rounded-md bg-green-50 px-2 py-1 text-xs font-medium text-green-700 ring-1 ring-inset ring-green-600/20">Desarrollador
+        full stack</label>
+      <h1 class="text-4xl font-bold tracking-tight text-white sm:text-6xl">
+        Descubre mi creatividad<br>en un solo vistazo
+      </h1>
+      <p class="mt-6 text-lg leading-8 text-gray-400">Creador implacable. Diseño con actitud, codifico con precisión. <br>
+        Explora mi portafolio web y descubre la fuerza detrás<br>de mis creaciones.</p>
+      <p href="http://" class="my-12 text-lg font-semibold text-white">Hablemos <span aria-hidden="true">→</span></p>
+    </div>
+    <div class="flex gap-12">
+      <div class="flex gap-2">
+        <h1 class="text-5xl text-white">3</h1>
+        <p class="text-gray-400">Años de<br>experiencias</p>
+      </div>
+      <div class="flex gap-2">
+        <h1 class="text-5xl text-white">15</h1>
+        <p class="text-gray-400">Proyectos<br>terminados</p>
+      </div>
+    </div>
+  </div>
+</template>
